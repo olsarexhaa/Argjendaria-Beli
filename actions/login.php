e82a66ae09dc6a9705cb271bfb4c20004f33d1fb
olsarexhaa <149424803+olsarexhaa@users.noreply.github.com>
2024-03-05 02:20:06 +0100

    log in dhe log out si admin

---
+++ actions/login.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../backend/Controllers/UserController.php';
+
+$userController = new UserController();
+
+$userController ->login()
+
+?>
---
+++ actions/logout.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../backend/Controllers/UserController.php';
+
+$userController = new UserController();
+
+$userController->logout();
